51e49e614b8d3640fad183616dadba0656ebd366
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2023-02-13 11:30:55 +0330

    edit hero section

--- resources/views/components/home/hero.blade.php
+++ resources/views/components/home/hero.blade.php
@@ -1,72 +1,83 @@
 {{--Hero Section Start--}}
-<div class="relative pt-[60px] lg:pt-[80px] pb-[110px] bg-white dark:bg-slate-800">
-  <div class="container">
-    <div class="flex flex-wrap -mx-4">
-      <div class="w-full lg:w-5/12 px-4">
+<div class="relative bg-white dark:bg-slate-800 pt-[60px] pb-[110px] lg:pt-[80px]">
+  <div class="container mx-auto">
+    <div class="-mx-4 flex flex-wrap">
+      <div class="w-full px-4 lg:w-5/12">
         <div class="hero-content">
-          <h1 class="text-dark dark:text-gray-200 font-bold text-4xl sm:text-[42px] lg:text-[40px] xl:text-[42px] leading-snug mb-6">
-            My Name Is Sajjad
+          <h1
+            class="mb-3 text-4xl font-bold leading-snug text-dark dark:text-gray-200 sm:text-[42px] lg:text-[40px] xl:text-[42px]"
+          >
+            My Name Is <br>
+            Sajjad Khalafi
           </h1>
-          <p class="text-base mb-8 max-w-[520px]">
+          <p class="mb-8 max-w-[480px] text-base text-body-color">
             I am Junior Backend Developer. <br>
-            I Love to Build amazing websites that why I learn programming
+            I always wanted to make a website, that's why I learned programming.
           </p>
-          <ul class="flex flex-wrap items-center">
-            <li>
-              <x-button-link href="#portfolio" variant="primary" class="rounded-lg">
-                My Projects
-              </x-button-link>
-            </li>
-          </ul>
+
+          <x-button-link href="#portfolio" variant="primary" class="rounded-lg">
+            My Projects
+          </x-button-link>
+
           <div class="clients pt-16">
-            <h6 class="font-normal text-xs flex items-center text-body-color dark:text-gray-300 mb-4">
+            <h6
+              class="mb-2 flex items-center text-xs font-normal text-body-color dark:text-gray-300"
+            >
               Follow me on Social Media
-              <span class="w-8 h-[1px] bg-body-color inline-block ml-2"></span>
+              <span
+                class="ml-2 inline-block h-[1px] w-8 bg-body-color"
+              ></span>
             </h6>
-            <x-social-icons />
+
+            <x-social-icons class="py-3" />
+
           </div>
         </div>
       </div>
-      <div class="hidden lg:block lg:w-1/12 px-4"></div>
-      <div class="w-full lg:w-6/12 px-4">
-        <div class="lg:text-right lg:ml-auto">
-          <div class="relative inline-block z-10 pt-11 lg:pt-0">
-            <img src="{{ url('img/me.jpg') }}" alt="hero" class="max-w-full lg:ml-auto" />
-            <span class="absolute -left-8 z-[-1]">
-              <svg
-                width="93"
-                height="93"
-                viewBox="0 0 93 93"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3"/>
-                <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3"/>
-              </svg>
-            </span>
+      <div class="hidden px-4 lg:block lg:w-1/12"></div>
+      <div class="w-full px-4 lg:w-6/12">
+        <div class="lg:ml-auto lg:text-right">
+          <div class="relative z-10 inline-block pt-11 lg:pt-0">
+            <img
+              src="{{ url('img/hero-image-01.png') }}"
+              alt="hero"
+              class="max-w-full lg:ml-auto"
+            />
+            <span class="absolute -left-8 -bottom-8 z-[-1]">
+                  <svg
+                    width="93"
+                    height="93"
+                    viewBox="0 0 93 93"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
+                    <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
+                  </svg>
+                </span>
           </div>
         </div>
       </div>
